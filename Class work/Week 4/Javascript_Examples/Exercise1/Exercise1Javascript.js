34e1e46ef6a2b6f1436fcf6b60f706136933a635
Ryan-rc-Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2017-11-13 12:32:30 +0000

    Started Week 4 work

---
+++ Class work/Week 4/Javascript_Examples/Exercise1/Exercise1Javascript.js
@@ -0,0 +1,19 @@
+function AlertMessage()
+{
+  alert("You have been Alerted!")
+}
+
+
+function ConsoleMessage()
+{
+  console.log("I am a console message")
+}
+
+
+function WhatsMyName()
+{
+  var firstName = "Jon"
+  var lastName = "Brown"
+
+  console.log(firstName + " " + lastName)
+}
---
+++ Class work/Week 4/Javascript_Examples/Exercise2/Exercise2Javascript.js
@@ -0,0 +1,24 @@
+
+function MakeTextSmaller()
+{
+	document.getElementById("textSize").style.fontSize = "10px"
+}
+
+
+function ReplaceText()
+{
+	document.getElementById("placeholderText").innerHTML = "My new text!"
+
+}
+
+
+
+function TurnLightOn()
+{
+	document.getElementById("lightBulb").img src = 'img/bulb-on.png'
+}
+
+function TurnLightOff()
+{
+
+}
