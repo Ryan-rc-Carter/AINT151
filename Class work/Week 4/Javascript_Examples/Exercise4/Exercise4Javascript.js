4a835a8f3f2c3ee100a9da5a685a1d947079b0e3
Ryan-rc-Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2017-11-16 10:05:58 +0000

    Completed Week 4 Work

--- Class work/Week 4/Javascript_Examples/Exercise4/Exercise4Javascript.js
+++ Class work/Week 4/Javascript_Examples/Exercise4/Exercise4Javascript.js
@@ -1,5 +1,32 @@
 function CreatePlayer()
 {
-  document.getElementById('name').innerHTML = 
+  var playerName = document.getElementById('playername').value;
+  var playerColour = document.getElementById('playercolour').value;
+  var playerHealth = document.getElementById('playerhealth').value;
+  var playerWeapon = document.getElementById('playerweapon').value;
+
+  console.log(playerName)
+  console.log(playerWeapon)
+  console.log(playerColour)
+
+  if (playerWeapon == 1)
+  {
+    var weaponType = 'Crossbow of much hurting'
+  }
+  else if (playerWeapon == 2)
+  {
+    var weaponType = 'Broadsword of so slicing'
+  }
+  else
+  {
+    var weaponType = 'Wand of amaze magics'
+  }
+
+  document.getElementById('name').innerHTML = playerName;
+  document.getElementById('colour').style.backgroundColor = playerColour;
+  document.getElementById('health').innerHTML = playerHealth;
+  document.getElementById('weapon').innerHTML = weaponType;
+
+
 
 }
